b3463b0fe0f5ad7abed37411da98f56c35a7857e
Alvin Lee <alvin@ootbdev.com>
2025-02-02 12:41:32 -0700

    Adds logging messages and template pem key

--- app/src/main/java/com/example/iapdemo/MainActivity.kt
+++ app/src/main/java/com/example/iapdemo/MainActivity.kt
@@ -32,8 +32,7 @@ import com.amazon.device.iap.model.PurchaseUpdatesResponse
 import com.example.iapdemo.databinding.ActivityMainBinding
 import java.util.*
 
-
-const val parentSKU = "com.amazon.sample.iap.subscription.mymagazine"
+const val parentSKU = "techsubscription"
 
 class MainActivity : AppCompatActivity() {
 
@@ -49,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
 
         PurchasingService.registerListener(this, purchasingListener)
+        Log.v("KOTLIN_INTEGRATION", "Registering PurchasingListener")
 
         binding.subscriptionButton.setOnClickListener { PurchasingService.purchase(parentSKU) }
 
@@ -64,66 +64,79 @@ class MainActivity : AppCompatActivity() {
         PurchasingService.getPurchaseUpdates(true)
 
         //getProductData will validate the SKUs with Amazon Appstore
-        val productSkus = hashSetOf(parentSKU)
+        val productSkus = hashSetOf("techquarterly","techmonthly")
 
         PurchasingService.getProductData(productSkus)
-        Log.v("Validating SKUs", "Validating SKUs with Amazon")
+        Log.v("KOTLIN_INTEGRATION", "Validating SKUs with Amazon")
     }
 
     private var purchasingListener: PurchasingListener = object : PurchasingListener {
         override fun onUserDataResponse(response: UserDataResponse) {
+            Log.v("KOTLIN_INTEGRATION", "onUserDataResponse")
             when (response.requestStatus) {
                 UserDataResponse.RequestStatus.SUCCESSFUL -> {
                     currentUserId = response.userData.userId
                     currentMarketplace = response.userData.marketplace
+                    Log.v("KOTLIN_INTEGRATION", response.userData.toString())
                 }
                 UserDataResponse.RequestStatus.FAILED, UserDataResponse.RequestStatus.NOT_SUPPORTED, null -> {
-                    Log.e("Request", "Request error")
+                    Log.e("KOTLIN_INTEGRATION", "Request error")
                 }
             }
         }
 
         override fun onProductDataResponse(productDataResponse: ProductDataResponse) {
+            Log.v("KOTLIN_INTEGRATION", "onProductDataResponse")
             when (productDataResponse.requestStatus) {
                 ProductDataResponse.RequestStatus.SUCCESSFUL -> {
+                    Log.v("KOTLIN_INTEGRATION", "ProductDataResponse.RequestStatus SUCCESSFUL")
                     val products = productDataResponse.productData
                     for (key in products.keys) {
                         val product = products[key]
                         Log.v(
-                            "Product:",
+                            "KOTLIN_INTEGRATION",
                             "Product: ${product!!.title} \n Type: ${product.productType}\n SKU: ${product.sku}\n Price: ${product.price}\n Description: ${product.description}\n"
                         )
                     }
                     for (s in productDataResponse.unavailableSkus) {
-                        Log.v("Unavailable SKU:$s", "Unavailable SKU:$s")
+                        Log.v("KOTLIN_INTEGRATION", "Unavailable SKU:$s")
                     }
                 }
-                ProductDataResponse.RequestStatus.FAILED -> Log.v("FAILED", "FAILED")
+                ProductDataResponse.RequestStatus.FAILED -> Log.v("KOTLIN_INTEGRATION", "ProductDataResponse.RequestStatus FAILED")
 
                 else -> {
-                    Log.e("Product", "Not supported")
+                    Log.e("KOTLIN_INTEGRATION", "Not supported")
                 }
             }
         }
 
         override fun onPurchaseResponse(purchaseResponse: PurchaseResponse) {
+            Log.v("KOTLIN_INTEGRATION", "onPurchaseResponse")
             when (purchaseResponse.requestStatus) {
-                PurchaseResponse.RequestStatus.SUCCESSFUL -> PurchasingService.notifyFulfillment(
-                    purchaseResponse.receipt.receiptId,
-                    FulfillmentResult.FULFILLED
-                )
+                PurchaseResponse.RequestStatus.SUCCESSFUL -> {
+                    Log.v("KOTLIN_INTEGRATION", "PurchaseResponse.RequestStatus SUCCESSFUL")
+                    Log.v("KOTLIN_INTEGRATION", purchaseResponse.receipt.toString())
+                    PurchasingService.notifyFulfillment(
+                        purchaseResponse.receipt.receiptId,
+                        FulfillmentResult.FULFILLED
+                    )
+                }
                 PurchaseResponse.RequestStatus.FAILED -> {
+                  Log.v("KOTLIN_INTEGRATION", "PurchaseResponse.RequestStatus FAILED")
                 }
                 else -> {
-                    Log.e("Product", "Not supported")
+                    Log.e("KOTLIN_INTEGRATION", "Not supported")
                 }
             }
         }
 
         override fun onPurchaseUpdatesResponse(response: PurchaseUpdatesResponse) {
+            Log.v("KOTLIN_INTEGRATION", "onPurchaseUpdatesResponse")
             when (response.requestStatus) {
                 PurchaseUpdatesResponse.RequestStatus.SUCCESSFUL -> {
+                    Log.v("KOTLIN_INTEGRATION", "PurchaseUpdatesResponse.RequestStatus SUCCESSFUL")
                     for (receipt in response.receipts) {
+                        Log.v("KOTLIN_INTEGRATION", receipt.toString())
                         if (!receipt.isCanceled) {
                             binding.textView.apply {
                                 text = "SUBSCRIBED"
@@ -135,9 +148,11 @@ class MainActivity : AppCompatActivity() {
                         PurchasingService.getPurchaseUpdates(true)
                     }
                 }
-                PurchaseUpdatesResponse.RequestStatus.FAILED -> Log.d("FAILED", "FAILED")
+                PurchaseUpdatesResponse.RequestStatus.FAILED -> {
+                  Log.v("KOTLIN_INTEGRATION", "PurchaseUpdatesResponse.RequestStatus FAILED")
+                }
                 else -> {
-                    Log.e("Product", "Not supported")
+                    Log.e("KOTLIN_INTEGRATION", "Not supported")
                 }
             }
         }
